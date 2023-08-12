2e391c01e97affc914a4f35b7f3ebf96abbdc71f
Romenildo Ferreira <romenildoferreira@gmaill.com>
2023-08-12 16:05:52 -0300

    home style and list of foods

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -8,6 +8,10 @@ export class FoodService {
   constructor() { }
 
   getAll():string[] {
-    return ["Item 1", "Item 2", "item 3"]
+    return ["https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2023/01/comidas-da-regiao-do-nordeste-moqueca-maranhense.jpg",
+             "https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2023/02/coxinha-g772e5ca7a_1920.jpg", 
+             "https://a.cdn-hotels.com/gdcs/production101/d809/aa4c6796-747a-4fbf-8b76-f90d6252dfa2.jpg",
+            "https://www.montarumnegocio.com/wp-content/uploads/2021/05/Comidas-nordestinas-para-vender.jpg"
+          ]
   }
 }
